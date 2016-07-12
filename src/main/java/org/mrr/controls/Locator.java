61d53a301b7f2ff39af1d1a54d74cff691677e92
Marian Wamsiedel <marian.wamsiedel@pentasys.de>
2016-07-12 22:27:24 +0200

    Locator class: lombok annotations to replace equals, hashcode and toString

--- src/main/java/org/mrr/controls/Locator.java
+++ src/main/java/org/mrr/controls/Locator.java
@@ -1,20 +1,21 @@
 package org.mrr.controls;
 
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.mrr.IdentificationType;
 
 /**
  * The class stores the information needed to locate a control on a html page.
  */
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
 public class Locator {
     private final IdentificationType type;
     private final String id;
 
-    public Locator(final IdentificationType type, final String id) {
-        this.type = type;
-        this.id = id;
-    }
-
-    public IdentificationType type() {
+    IdentificationType type() {
         return type;
     }
 
@@ -22,30 +23,4 @@ public class Locator {
         return id;
     }
 
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Locator locator = (Locator) o;
-
-        if (type != locator.type) return false;
-        return id != null ? id.equals(locator.id) : locator.id == null;
-
-    }
-
-    @Override
-    public int hashCode() {
-        int result = type != null ? type.hashCode() : 0;
-        result = 31 * result + (id != null ? id.hashCode() : 0);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Locator{" +
-                "identificationType=" + type +
-                ", id='" + id + '\'' +
-                '}';
-    }
 }
