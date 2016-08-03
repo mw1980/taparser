39384ed8b2523c2b91d34aa14760fa1d7533d4c4
Marian Wamsiedel <marian.wamsiedel@pentasys.de>
2016-08-03 22:23:01 +0200

    caching for the controls repository implemented

--- src/main/java/org/mrr/controls/ControlsRepositoryImpl.java
+++ src/main/java/org/mrr/controls/ControlsRepositoryImpl.java
@@ -6,32 +6,47 @@ import org.mrr.controls.api.UiControl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.HashMap;
 import java.util.Map;
 
 /**
  * The class stores the controls registered for the current application.
- * //TODO: caching here still missing.
  */
 @Component
-public class ControlsRepositoryImpl implements ControlsRepository {
+class ControlsRepositoryImpl implements ControlsRepository {
 
     private final ControlsAgent agent;
 
+    //low level caching.
+    private Map<String, UiControl> controls;
+
     @Autowired
     public ControlsRepositoryImpl(final ControlsAgent agent) {
+        this.controls = new HashMap<>();
         this.agent = agent;
     }
 
     @Override
     public Map<String, UiControl> controls() {
-        return agent.supply();
+        initControlsIfNeeded();
+        return controls;
+    }
+
+    private void initControlsIfNeeded() {
+        if (noControlsAvailable()) {
+            this.controls = agent.supply();
+        }
+    }
+
+    private boolean noControlsAvailable() {
+        return this.controls.isEmpty();
     }
 
     @Override
     public UiControl searchControlByName(final String name) {
-        final Map<String, UiControl> controls = agent.supply();
-        if (controls.containsKey(name)) {
-            return controls.get(name);
+        initControlsIfNeeded();
+        if (this.controls.containsKey(name)) {
+            return this.controls.get(name);
         } else {
             return UiControl.NO_CONTROL;
         }
--- src/test/java/org/mrr/controls/ControlsRepositoryImplTest.java
+++ src/test/java/org/mrr/controls/ControlsRepositoryImplTest.java
@@ -1,28 +1,89 @@
 package org.mrr.controls;
 
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
-import org.mrr.controls.api.LoadControlsException;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mrr.controls.api.ControlsAgent;
+import org.mrr.controls.api.UiControl;
+import org.mrr.controls.api.UiLocator;
 
-import static org.mrr.reader.TestConstants.TEST_RESOURCE_FOLDER_LOCATION;
+import java.util.Map;
+
+import static java.util.Collections.singletonMap;
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.mrr.IdentificationType.ID;
 
 public class ControlsRepositoryImplTest {
 
-    @Test
-    @Ignore
-    public void whenReadingCorrectIdentifiers_shouldSaveThemAsExpected() {
-        final String filePath = TEST_RESOURCE_FOLDER_LOCATION + "ElementIdentifiers.csv";
+    private static final String CONTROL_NAME = "name";
 
-        //TODO: mock hier the controlAgent after switching to spring.
-        //Map<String, UiControl> elements = new ControlsRepositoryImpl(new ControlsAgentImpl(filePath)).controls();
-        //assertThat(elements.get("name")).isEqualTo(new UiControl("name",
-        //        new UiLocator(IdentificationType.ID, "userNameHtmlId")));
+    @Mock
+    private ControlsAgent agent;
+
+    @Before
+    public void setup(){
+        MockitoAnnotations.initMocks(this);
     }
 
-    @Test(expected = LoadControlsException.class)
-    @Ignore
-    public void whenReadingMalformedIdentifiers_shouldThrowTestSpecificationReaderException() {
-        final String location = TEST_RESOURCE_FOLDER_LOCATION + "MalformedIdentifiers.csv";
-        //new ControlsRepositoryImpl(new ControlsAgentImpl(location)).controls();
+    @Test
+    public void whenAskingForControls_shouldCallTheControlsAgent(){
+        final ControlsRepositoryImpl repository = new ControlsRepositoryImpl(this.agent);
+        repository.controls();
+        verify(agent).supply();
+    }
+
+    @Test
+    public void whenAskingForControls_shouldCashTheResult(){
+        final ControlsRepositoryImpl repository = new ControlsRepositoryImpl(this.agent);
+        when(agent.supply()).thenReturn(notEmptyControlsMap());
+        //the second call should return the cached values, not call the controls agent for the second time.
+        repository.controls();
+        repository.controls();
+        verify(agent).supply();
+    }
+
+    private Map<String, UiControl> notEmptyControlsMap() {
+        return singletonMap(CONTROL_NAME, notEmptyControl());
+    }
+
+    private UiControl notEmptyControl() {
+        return new UiControl(CONTROL_NAME, new UiLocator(ID, "nameTF"));
+    }
+
+    @Test
+    public void whenSearchingForControlByName_shouldCallTheControlsAgent(){
+        final ControlsRepositoryImpl repository = new ControlsRepositoryImpl(this.agent);
+        repository.searchControlByName("");
+        verify(agent).supply();
+    }
+
+    @Test
+    public void whenSearchingForControlByName_shouldCachTheResult(){
+        final ControlsRepositoryImpl repository = new ControlsRepositoryImpl(this.agent);
+        when(agent.supply()).thenReturn(notEmptyControlsMap());
+        repository.searchControlByName("");
+        repository.searchControlByName("");
+        verify(agent).supply();
+    }
+
+    @Test
+    public void whenControlNoFound_shouldReturnTheNoControlUiObject(){
+        final ControlsRepositoryImpl repository = new ControlsRepositoryImpl(this.agent);
+        assertEquals(
+                UiControl.NO_CONTROL,
+                repository.searchControlByName("name"));
+    }
+
+    @Test
+    public void whenSearchingForAvailableObject_shouldReturnCorrectObject(){
+        when(agent.supply()).thenReturn(notEmptyControlsMap());
+        final ControlsRepositoryImpl repository = new ControlsRepositoryImpl(this.agent);
+        assertEquals(
+                notEmptyControl(),
+                repository.searchControlByName(CONTROL_NAME));
+
     }
 }
